ebabc9e8f299c405590fc534b5a7b71a520b7124
Pratik Thakur <thakurpratik246@gmail.com>
2026-09-20 01:12:16 +0530

    Time: 0 ms (100%), Space: 8.1 MB (42.17%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
-
-    bool isPivot(int x , int sum , int n) {
-        int sum1 = 0 ;
-        for(int j = x ; j <= n ; j++) {
-            sum1 += j ;
-        }
-        if(sum == sum1) {
-            return true ;
-        }
-        return false ;
-    }
-
     int pivotInteger(int n) {
-        if(n == 1) return 1 ;
+        int sum = n * (n + 1) / 2;
+        int x = sqrt(sum);
 
-        int sum = 0 ;
-        for(int i = 1 ; i < n ; i++) {
-            sum += i ;
-            if(isPivot(i , sum , n)) {
-                return i ;
-            }
-        }
+        if (x * x == sum)
+            return x;
 
-        return -1 ;
+        return -1;
     }
 };
