b746837d73ef153a17c2da70f495adbe0de15942
Pratik Thakur <thakurpratik246@gmail.com>
2026-09-20 01:06:55 +0530

    Time: 23 ms (11.48%), Space: 8.1 MB (71.85%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -15,15 +15,14 @@ public:
     int pivotInteger(int n) {
         if(n == 1) return 1 ;
 
-        int x , sum = 0 ;
+        int sum = 0 ;
         for(int i = 1 ; i < n ; i++) {
             sum += i ;
-            x = i ;
-            if(isPivot(x , sum , n)) {
-                return x ;
+            if(isPivot(i , sum , n)) {
+                return i ;
             }
         }
-        
+
         return -1 ;
     }
 };
